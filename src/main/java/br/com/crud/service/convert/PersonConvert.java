fcdd0cd39b24ce17625d5a31db78b057883aa966
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-16 21:27:32 -0300

    leaving the responsibility of summoning builder to lombok

--- src/main/java/br/com/crud/service/convert/PersonConvert.java
+++ src/main/java/br/com/crud/service/convert/PersonConvert.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import br.com.crud.domain.model.Person;
 import br.com.crud.dto.PersonDataTransferObject;
 import br.com.crud.service.exception.PersonNotFoundException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class PersonConvert {
-
-  private PersonConvert() {}
+  static final String MESSAGE_NOT_FOUND_EXCEPTION = "Person not found!";
 
   public static Person convert(PersonDataTransferObject dto) {
     return Person.newBuilder().fullName(dto.getFullName()).identifier(dto.getIdentifier())
